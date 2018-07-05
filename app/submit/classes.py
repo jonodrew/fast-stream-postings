446676207e987f47e6dd0e48492b47836d9cc302
Jonathan Kerr <jonathan.kerr@digital.cabinet-office.gov.uk>
2018-07-05 11:24:37 +0100

    Completes check answer page

--- app/submit/classes.py
+++ app/submit/classes.py
@@ -1,8 +1,9 @@
 from typing import Dict, List
+import json
 
 
 class RoleQuestion:
-    def __init__(self, name: str, skills: Dict[str, int]):
+    def __init__(self, name: str, skills: Dict[str, str]):
         self.name = name
         self.skills = skills
 
@@ -11,3 +12,12 @@ class Family:
     def __init__(self, name: str, roles: List[RoleQuestion]):
         self.name = name
         self.roles = roles
+
+
+def skill_dump(form) -> str:
+    skills = {
+        'Skills gained': ' | '.join(form.getlist('skills')),
+        'Overall level attained': form['skill-level'],
+        'How gained': form['skills-describe']
+    }
+    return json.dumps(skills)
